f2c9f3053147ef1f28eaca1668b4fb3c9f2d936c
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2020-05-27 11:19:08 +0300

    Настройка генерации

---
+++ src/utils.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const shuffle = (array) => {
+  const resultArray = array.slice();
+  for (let i = resultArray.length - 1; i > 0; i--) {
+    const randomNumber = Math.floor(Math.random() * (i + 1));
+    [resultArray[randomNumber], resultArray[i]] = [resultArray[i], resultArray[randomNumber]];
+  }
+
+  return resultArray;
+};
+
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports = {
+  shuffle,
+  getRandomInt,
+};
